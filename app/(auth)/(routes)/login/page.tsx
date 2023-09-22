f7371c8f76ac94810b799343cde369ca2d55b15c
An Phan <100254006+phanhuyan@users.noreply.github.com>
2023-09-21 21:23:37 -0500

    first commit

---
+++ app/(auth)/(routes)/login/page.tsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+    return (
+        <div>
+            Login Page (Protected)
+        </div>
+      );
+}
+ 
+export default LoginPage;
---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,9 @@
+const rootPage = () => {
+    return (
+        <div>
+            Root Page (Protected)
+        </div>
+      );
+}
+ 
+export default rootPage;
